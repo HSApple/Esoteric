b22f9bc229db8307fe1cafefe7fd7144bce7e02d
arendon2 <arendon2018@outlook.com>
2019-07-10 13:04:17 -0500

    loginsetup

---
+++ LoginSetup/FirebaseApp-1-Email-Authentication-Start/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  FirebaseApp
+//
+//  Created by student on 7/10/19.
+//  Copyright © 2019 Robert Canton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ target: UIBarButtonItem) {
+    }
+}
