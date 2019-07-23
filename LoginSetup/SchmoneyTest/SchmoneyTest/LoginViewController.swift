f65e89bdce24fbf1e0db6336464eea4e90949724
arendon2 <arendon2018@outlook.com>
2019-07-23 12:35:29 -0500

    saving

--- LoginSetup/SchmoneyTest/SchmoneyTest/LoginViewController.swift
+++ LoginSetup/SchmoneyTest/SchmoneyTest/LoginViewController.swift
@@ -2,8 +2,48 @@
 //  LoginViewController.swift
 //  SchmoneyTest
 //
-//  Created by student on 7/22/19.
+//  Created by student on 7/23/19.
 //  Copyright © 2019 Esoteric. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func loginAction(_ sender: Any) {
+    }
+    Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            if error == nil{
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- LoginSetup/SchmoneyTest/SchmoneyTest/SignUpViewController.swift
+++ LoginSetup/SchmoneyTest/SchmoneyTest/SignUpViewController.swift
@@ -2,8 +2,57 @@
 //  SignUpViewController.swift
 //  SchmoneyTest
 //
-//  Created by student on 7/22/19.
+//  Created by student on 7/23/19.
 //  Copyright © 2019 Esoteric. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UITextField!
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
